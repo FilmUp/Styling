6342d1205466dd78f81d6293c8d676638047cd03
fletchiez <elffletch13@gmail.com>
2015-11-29 12:43:17 -0500

    First three done

--- js/js.js
+++ js/js.js
@@ -1,12 +1,34 @@
-$(document).ready(function(){
-        $('#logbutton').on('click', function(){
-            $("#signin").addClass('hidden');
-            $("#login").removeClass('hidden');
-        });
+$(document).ready(function () {
+    var p = $("#logbutton");
+    var offset = p.offset();
+    $(".arrow").offset({ top: offset.top+33, left: offset.left+30})
+        
+    $( window ).resize(function() {
+        if ($('#signin').hasClass("hidden")){
+            var p = $("#logbutton");
+            var offset = p.offset();
+            $(".arrow").offset({ top: offset.top+33, left: offset.left+30})
+        }
+        else{
+            var p = $("#signbutton");
+            var offset = p.offset();
+            $(".arrow").offset({ top: offset.top+33, left: offset.left+30})
+        }        
+    });
+        
+    $('#logbutton').on('click', function(){
+        $("#signin").addClass('hidden');
+        $("#login").removeClass('hidden');
+        var p = $("#logbutton");
+        var offset = p.offset();
+        $(".arrow").offset({ top: offset.top+33, left: offset.left+30});
+    });
 
-        $('#signbutton').on('click', function(){
-            $("#login").addClass('hidden');
-            $("#signin").removeClass('hidden');
-        });
-
-    })
+    $('#signbutton').on('click', function(){
+        $("#login").addClass('hidden');
+        $("#signin").removeClass('hidden');
+        var p = $("#signbutton");
+        var offset = p.offset();
+        $(".arrow").offset({ top: offset.top+33, left: offset.left+30})
+    });
+})
